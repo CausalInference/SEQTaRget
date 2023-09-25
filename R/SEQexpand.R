3c37f0438052e7654b659ca88231df0dd5bc6e46
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-09-25 12:11:25 -0400

    Parallelization of SEQexpand by ID

--- R/SEQexpand.R
+++ R/SEQexpand.R
@@ -6,44 +6,43 @@
 #' @param max Integer: maximum in \code{time_col} to expand about
 #' @param min Integer: minimum in \code{time_col} to expand about
 #'
-#' @import data.table
+#' @import data.table foreach doParallel
 #'
 #' @export
-
-id.col = "ID"; time.col = "month"; eligible.col = "eligible"
-SEQexpand <- function(data, id.col, time.col, eligible.col, min = NULL, max = NULL) {
+SEQexpand <- function(data, id.col, time.col, eligible.col, min = NULL, max = NULL, parallel, cores) {
   data <- as.data.table(data)
+  unique_ids <- unique(data[[id.col]])
 
   if(!is.null(min) && !is.null(max)){
     if(max < min) stop("The maximum is less than the minimum for expansion")
   }
   if(!is.null(min)) data <- data[get(time.col) >= min]
   if(!is.null(max)) data <- data[get(time.col) <= max]
+  if(parallel == TRUE){cl <- makeCluster(cores); registerDoParallel(cl)}
 
-  DT1 <- data[, `:=`(rev_row = .N - seq_len(.N)), by = get(id.col)
-              ][get(eligible.col) == 1
-                ][, rev_row := sort(rev_row), by = get(id.col)
-                  ][, .(time = seq.int(get(time.col), rev_row),
-                        trial = rep(.GRP - 1, rev_row - get(time.col) + 1)),
-                    by = c(id.col, rev_row)
-                    ][, setnames(.SD, old = "time", new = time.col)
-                      ][, -c('rev_row')
-                        ][, period := seq_len(.N) - 1, by = trial]
+  process_id <- function(id) {
+    DT_sub <- data[get(id.col) == id]
 
-  DT2 <- DT1[data, on = c(id.col, time.col)
-             ][, c('rev_row', eligible.col) := NULL]
+    DT1_sub <- DT_sub[, `:=`(rev_row = .N - seq_len(.N)), by = get(id.col)
+                      ][get(eligible.col) == 1
+                        ][, rev_row := sort(rev_row), by = get(id.col)
+                          ][, .(time = seq.int(get(time.col), rev_row),
+                                trial = rep(.GRP - 1, rev_row - get(time.col) + 1)),
+                            by = c(id.col, 'rev_row')
+                            ][, setnames(.SD, old = "time", new = time.col)
+                              ][, -c('rev_row')
+                                ][, period := seq_len(.N) - 1, by = trial]
+    DT2_sub <- DT1_sub[DT_sub, on = c(id.col, time.col)
+                       ][, c('rev_row', eligible.col) := NULL]
 
-  return(DT2)
+    return(DT2_sub)
+  }
+
+  resultDT <- foreach(id = unique_id, .combine = 'rbind', .packages = 'data.table') %dopar% {
+    process_id(id)
+  }
+
+  stopCluster(cl)
+
+  return(resultDT)
 }
-
-data[, `:=`(rev_row = .N - seq_len(.N)), by = get(id.col)
-  ][get(eligible.col) == 1
-  ][, rev_row := sort(rev_row), by = get(id.col)
-    ][, trial := .I - 1L
-      ][, .(time = seq.int(get(time.col), rev_row),
-            trial = trial),
-          by = .(get(id.col), rev_row)
-    ][, setnames(.SD, old = "time", new = time.col)
-      ][, -c('rev_row')
-        ][, period := seq_len(.N) - 1, by = trial]
-
