1dbfdd4fad8d193a042c35217426810b564b9fbd
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-10-11 13:45:48 -0400

    datafab and bool conversion fix

--- R/datafab.R
+++ R/datafab.R
@@ -120,3 +120,30 @@ for (patient_id in 1:N_PATIENTS) {
 
 print(dt)
 
+if(!require(pacman)) install.packages(pacman); pacman::p_load(data.table, foreach, doParallel)
+n_patients <- 1e5; max_time <- 59; ncores <- parallel::detectCores() - 1; doParallel::registerDoParallel(ncores)
+
+output <- foreach(i = 1:n_patients, .combine = "rbind", .packages = c("data.table")) %dopar% {
+  set.seed(1636+i)
+  drop_time <- sample(0:max_time, 1)
+  eligible_time <- sample(0:drop_time, 1)
+  sex <- rbinom(1, 1, 0.5)
+
+  ID <- data.table(ID = rep(i, drop_time+1),
+                   time = 0:drop_time,
+                   eligible = c(rep(1, eligible_time), rep(0, drop_time-eligible_time+1)),
+                   sex = rep(sex, drop_time+1),
+                   N = rnorm(length(0:(drop_time)), 10, 5),
+                   L = runif(1),
+                   P = runif(1, 9, 10))
+
+  ID[, `:=`(L = L[1] * cumprod(c(1, rep(1.04, .N-1))),
+            P = P[1] * cumprod(c(1, rep(0.98, .N-1))),
+            tx_init = as.integer(eligible == 1 &
+                                   shift(eligible, type = "lag", fill = 1) == 1 &
+                                   shift(eligible, type = "lead", fill = 1) == 0))]
+
+  return(ID)
+
+}
+write.csv(output, "data.csv")
--- R/internal_preprocessing.R
+++ R/internal_preprocessing.R
@@ -1,6 +1,6 @@
 convert.to.bool <- function(DT){
   binary.cols <- names(DT)[sapply(DT, function(col) all(unique(col) %in% c(0, 1)))]
-  DT[, (binary_cols) := lapply(.SD, as.logical), .SDcols = binary.cols]
+  DT[, (binary.cols) := lapply(.SD, as.logical), .SDcols = binary.cols]
 
   return(DT)
 }
