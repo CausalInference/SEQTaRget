42f8de0d7852c9e98a6b7844599d3b86220e74a3
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-11-08 13:35:12 -0500

    Update datafab.R

--- R/datafab.R
+++ R/datafab.R
@@ -1,4 +1,4 @@
-if(!require(pacman)) install.packages(pacman); pacman::p_load(data.table, foreach, doParallel, doRNG, survival, ggplot2, ggfortify, SEQuential)
+#if(!require(pacman)) install.packages(pacman); pacman::p_load(data.table, foreach, doParallel, doRNG, survival, ggplot2, ggfortify, SEQuential)
 
 gen_data <- function(){
   n_patients <- 1e3; max_time <- 59; ncores <- parallel::detectCores() - 1; cl <- makeCluster(ncores); registerDoParallel(cl)
@@ -36,10 +36,10 @@ gen_data <- function(){
   stopCluster(cl)
   return(output)
 }
-myparams <- SEQopts(covariates = "tx_init_bas+ tx_init_bas*period + tx_init_bas*period_sq + period + period_sq + sex + N_bas + L_bas + P_bas")
-data <- gen_data()
-test <- SEQuential::SEQuential(data, "ID", "time", "eligible", "tx_init", "outcome", method = "ITT", myparams)
-print(test$coefficients)
+#myparams <- SEQopts(covariates = "tx_init_bas+ tx_init_bas*period + tx_init_bas*period_sq + period + period_sq + sex + N_bas + L_bas + P_bas")
+#data <- gen_data()
+#test <- SEQuential::SEQuential(data, "ID", "time", "eligible", "tx_init", "outcome", method = "ITT", myparams)
+#print(test$coefficients)
 
 #Debugging Junk ==========
 #id.col = "ID"; time.col = "time"; eligible.col = "eligible"; outcome.col = "outcome"; treatment.col = "tx_init"; method = "ITT"
