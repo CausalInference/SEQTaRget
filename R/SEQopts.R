c8df02cae1826a758dab580cabd12cc55056c14c
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-11-08 12:06:34 -0500

    added _sq

--- R/SEQopts.R
+++ R/SEQopts.R
@@ -5,7 +5,6 @@
 #' @param bootstrap Logical: defines if SEQuential should run bootstrapping, default is FALSE
 #' @param nboot Integer: number of bootstraps
 #' @param seed Integer: starting seed
-#' @param memory Integer: number of bytes for in-memory use, default is 1GB less than free system max
 #' @param max Integer: maximum time to expand about
 #' @param expand Logical: defines if the data should be expanded, default is TRUE
 #' @param covariates String: covariates to coerce into a formula object, eg. "A+B*C"
@@ -14,16 +13,16 @@
 #'
 #' @export
 SEQopts <- function(parallel = FALSE, nthreads = data.table::getDTthreads(), ncores = parallel::detectCores() - 1,
-                    bootstrap = FALSE, nboot = 100, seed = 1636, memory = get_ram() - 2^20,
+                    bootstrap = FALSE, nboot = 100, seed = 1636,
                     max = Inf, expand = TRUE, covariates = NA, weighted = FALSE, stabilized = FALSE,
-                    weight.time = "post", baseline.indicator = "_bas"){
+                    weight.time = "post", baseline.indicator = "_bas", sq.indicator = "_sq"){
+
+  covariates <- gsub(" ", "", covariates)
 
   params <- list(parallel = parallel, nthreads = nthreads, ncores = ncores, bootstrap = bootstrap,
-                 nboot = nboot, seed = seed, memory = memory, max = max, expand = expand, covariates = covariates,
+                 nboot = nboot, seed = seed, max = max, expand = expand, covariates = covariates,
                  weighted = weighted, stabilized = stabilized, weight.time = weight.time,
-                 baseline.indicator = baseline.indicator)
+                 baseline.indicator = baseline.indicator, sq.indicator = sq.indicator)
 
   return(params)
 }
-
-
--- R/internal_expand.R
+++ R/internal_expand.R
@@ -28,8 +28,11 @@ internal.expansion <- function(DT, id.col, time.col, eligible.col, outcome.col, 
 
   vars <- unlist(strsplit(opts$covariates, "\\+|\\*"))
   vars.base <- vars[grep(opts$baseline.indicator, vars)]
+  vars.sq <- vars[grep(opts$sq.indicator, vars)]
+
   vars.time <- vars[!vars %in% vars.base]
-  vars.base <- sub(opts$baseline.indicator, "", vars.base)
+  vars.base <- unique(sub(opts$baseline.indicator, "", vars.base))
+  vars.sq <- unique(sub(opts$sq.indicator, "", vars.sq))
   vars.kept <- c(vars, id.col, "trial", "period")
 
   data <- DT[(get(eligible.col)), .(period = Map(seq, get(time.col), table(DT[[id.col]])[.GRP] - 1)), by = eval(id.col)
@@ -39,6 +42,14 @@ internal.expansion <- function(DT, id.col, time.col, eligible.col, outcome.col, 
                    ]
 
   data_list <- list()
+  if(length(c(vars.time, vars.sq)) > 0){
+    data.time <- data[DT, on = c(id.col, "period" = time.col), .SDcols = vars.time
+                      ][, eval(eligible.col) := NULL
+                        ][, (paste0(vars.sq, opts$sq.indicator)) := lapply(.SD, function(x) x^2), .SDcols = vars.sq]
+
+    vars.found <- unique(c(vars.time, vars.sq, "period", "trial", id.col, outcome.col))
+    data_list[["time"]] <- data.time[, ..vars.found]
+  }
   if(length(vars.base) > 0){
     data.base <- data[DT, on = c(id.col, "trial" = time.col), .SDcols = vars.base, nomatch = 0
                       ][, eval(eligible.col) := NULL]
@@ -46,13 +57,6 @@ internal.expansion <- function(DT, id.col, time.col, eligible.col, outcome.col, 
     vars.found <- unique(c(paste0(vars.base, opts$baseline.indicator), "period", "trial", id.col))
     setnames(data.base, old = vars.base, new = paste0(vars.base, opts$baseline.indicator))
     data_list[["base"]] <- data.base[, ..vars.found]
-  }
-  if(length(vars.time) > 0){
-    data.time <- data[DT, on = c(id.col, "period" = time.col), .SDcols = vars.time
-                      ][, eval(eligible.col) := NULL]
-
-    vars.found <- unique(c(vars.time, "period", "trial", id.col, outcome.col))
-    data_list[["time"]] <- data.time[, ..vars.found]
   }
   if(length(data_list) > 1){
     out <- Reduce(function(x, y) merge(x, y, by = c(id.col, "trial", "period"), all = TRUE), data_list)
