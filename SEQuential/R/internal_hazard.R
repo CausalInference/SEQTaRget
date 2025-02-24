90df7abe817383b897eaabf361b2a3874a87b6b0
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-02-24 17:39:53 -0500

    minimized hazard sim

--- SEQuential/R/internal_hazard.R
+++ SEQuential/R/internal_hazard.R
@@ -7,43 +7,50 @@
 #' @importFrom survival finegray coxph Surv
 
 hazard <- function(model, params) {
-  set.seed(params@seed)
+  event <- firstEvent <- outcomeProb <- ce <- ceProb <- trial <- NULL
   subgroups <- if (!is.na(params@subgroup)) unique(params@data[[params@subgroup]]) else 1L
+  tx_bas <- paste0(params@treatment, params@indicator.baseline)
   for (i in seq_along(subgroups)) {
-    if (!is.na(params@subgroup)) cat("For subgroup:", subgroups[i], "\n")
-    cat("Simulation Initialized with:\n")
+    kept <- names(params@DT)[!names(params@DT) %in% c(paste0("followup", c("", params@indicator.squared)),
+                                                      paste0(params@treatment, c("", params@indicator.baseline)),
+                                                      "period", params@outcome)]
     
-    for (j in seq_along(params@time_varying)) {
-      item <- unlist(params@hazard.time_varying.dist[[i]][j])
-      cat(paste0(names(item), ": ", unlist(item), "\n\n"))
+    if (!is.na(params@compevent)) {
+      ce.data <- prepare.data(params@DT, params, case = "surv", type = "compevent")
+      ce.model <- fastglm.clean(fastglm(ce.data$X, ce.data$y, family = quasibinomial(link = "logit"), method = params@fastglm.method))
+      rm(ce.data)
     }
-    generators <- params@hazard.time_varying.dist[[i]]
-    functions <- generators[order(names(generators))]
-    tx_bas <- paste0(params@treatment, params@indicator.baseline)
+    
     handler <- function(data, params) {
-      trials <- copy(data)[, c(params@id, "trial", paste0("trial", params@indicator.squared), unlist(params@fixed)), with = FALSE
-                                ][, .SD[1], by = c(params@id, "trial")
-                                  ][, paste0(sort(unlist(params@time_varying)), params@indicator.baseline) := lapply(functions, function(f) f(.N))
-                                    ][rep(seq_len(.N), each = params@followup.max + 1)
-                                      ][, "followup" := seq.int(1:.N) - 1, by = c(params@id, "trial")
-                                        ][, paste0("followup", params@indicator.squared) := get("followup")^2]
+      trials <- copy(data)[, kept, with = FALSE
+                     ][, .SD[1], by = c(params@id, "trial")
+                       ][rep(seq_len(.N), each = params@followup.max + 1)
+                         ][, "followup" := seq.int(1:.N) - 1, by = c(params@id, "trial")
+                           ][, paste0("followup", params@indicator.squared) := get("followup")^2]
       
-      txTRUE <- copy(trials)[, eval(tx_bas) := 1
-                       ][, "prob" := inline.pred(model[[1]]$model[[i]]$model, newdata = .SD, params, type = "outcome")
-                         ][, "outcome" := rbinom(.N, 1, prob)
-                           ][, "firstEvent" := if (any(outcome == 1)) which(outcome == 1)[1] else .N, by = c(params@id, "trial")]
-      txFALSE <- copy(trials)[, eval(tx_bas) := 0
-                        ][, "prob" := inline.pred(model[[1]]$model[[i]]$model, newdata = .SD, params, type = "outcome")
-                          ][, "outcome" := rbinom(.N, 1, prob)
-                            ][, "firstEvent" := if (any(outcome == 1)) which(outcome == 1)[1] else .N, by = c(params@id, "trial")]
+      out <- lapply(list(txTRUE = copy(trials)[, eval(tx_bas) := 1],
+                      txFALSE = copy(trials)[, eval(tx_bas) := 0]), 
+                    function(x) {
+                      out <- x[, "outcomeProb" := inline.pred(model[[1]]$model[[i]]$model, newdata = .SD, params, type = "outcome")
+                               ][, "outcome" := rbinom(.N, 1, outcomeProb)]
+                      if (!is.na(params@compevent)) {
+                        out <- out[, "ceProb" := inline.pred(ce.model, newdata = .SD, params, case = "surv", type = "compevent")
+                                   ][, "ce" := rbinom(.N, 1, ceProb)
+                                     ][, "firstEvent" := if (any(outcome == 1 | ce == 1)) which(outcome == 1 | ce == 1)[1] else .N, by = c(params@id, "trial")]
+                      } else out <- out[, "firstEvent" := if (any(outcome == 1)) which(outcome == 1)[1] else .N, by = c(params@id, "trial")]
+                    })
+      
       rm(trials)
-      out <- rbind(txTRUE, txFALSE)
-      rm(txTRUE, txFALSE)
+      out <- rbindlist(out)
       data <- out[out[, .I[seq_len(firstEvent[1])], by = c(params@id, "trial", tx_bas)]$V1
                  ][, .SD[.N], by = c(params@id, "trial", tx_bas)
                    ][, firstEvent := NULL
                      ][, event := 0
                        ][outcome == 1, event := 1]
+      if (!is.na(params@compevent)) data <- data[ce == 1, event := 2][, ce := NULL]
+      
+      data <- data[, `:=` (outcome = NULL, event = factor(event, levels = c(0, 1, 2)))]
+      rm(out)
       
       if (!is.na(params@compevent)) {
         hr.data <- finegray(Surv(followup, event) ~ ., data, etype = 1)
@@ -57,7 +64,7 @@ hazard <- function(model, params) {
     bootstrap <- if (params@bootstrap) {
       UIDs <- unique(params@DT[[params@id]])
       lnID <- length(UIDs)
-      subDT <- subDT[, "trialID" := paste0(params@id, "_", trial)]
+      subDT <- params@DT[, "trialID" := paste0(params@id, "_", trial)]
 
       if (params@parallel) {
         setDTthreads(1)
@@ -65,9 +72,10 @@ hazard <- function(model, params) {
           id.sample <- sample(UIDs, round(params@bootstrap.sample * lnID), replace = TRUE)
           RMDT <- rbindlist(lapply(seq_along(id.sample), function(x) subDT[get(params@id) == id.sample[x], ]))
           handler(RMDT, params)
-        })
+        }, future.seed = params@seed)
       } else {
         out <- lapply(1:params@bootstrap.nboot, function(x) {
+          set.seed(params@seed + x)
           id.sample <- sample(UIDs, round(params@bootstrap.sample * lnID), replace = TRUE)
           RMDT <- rbindlist(lapply(seq_along(id.sample), function(x) subDT[get(params@id) == id.sample[x], ]))
           handler(RMDT, params)
--- SEQuential/tests/testthat/test_hazard.R
+++ SEQuential/tests/testthat/test_hazard.R
@@ -1,9 +1,6 @@
 test_that("Hazard and vcov", {
   data <- SEQdata
   model <- SEQuential(data, "ID", "time", "eligible", "tx_init", "outcome", list("N", "L", "P"), list("sex"),
-                      method = "ITT", options = SEQopts(calculate.var = TRUE, hazard = TRUE,
-                                                        hazard.time_varying.dist = list(list('N' = function(n) rnorm(n, 10, 1),
-                                                                  'L' = function(n) rnorm(n, 10, 1),
-                                                                  'P' = function(n) rnorm(n, 10, 1)))))
+                      method = "ITT", options = SEQopts(calculate.var = TRUE, hazard = TRUE))
   expect_s4_class(model, "SEQoutput")
 })
