df8e2f6cc95e4e983a6993aa593a105a01886594
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-06-09 21:13:52 -0400

    Testing for parallel session with 1 core
    Seems like macos runners can no longer multisession

--- SEQuential/tests/testthat/test_parallel.R
+++ SEQuential/tests/testthat/test_parallel.R
@@ -3,7 +3,7 @@ test_that("Parallelism, Bootstrapping, Output Class Methods", {
   model <- suppressWarnings(SEQuential(data, "ID", "time", "eligible", "tx_init", "outcome", list("N", "L", "P"), list("sex"),
     method = "dose-response", options = SEQopts(
       parallel = TRUE, weighted = TRUE,
-      bootstrap = TRUE, bootstrap.nboot = 2, ncores = 2
+      bootstrap = TRUE, bootstrap.nboot = 2, ncores = 1
     )
   ))
 
@@ -15,7 +15,7 @@ test_that("Non-Parallel Bootstrapping", {
   model <- suppressWarnings(SEQuential(data, "ID", "time", "eligible", "tx_init", "outcome", list("N", "L", "P"), list("sex"),
     method = "dose-response", options = SEQopts(
       parallel = FALSE, weighted = TRUE,
-      bootstrap = TRUE, bootstrap.nboot = 2, ncores = 2
+      bootstrap = TRUE, bootstrap.nboot = 2, ncores = 1
     )
   ))
 
