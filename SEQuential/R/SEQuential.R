0478c23f004aebc794b1e71bd531ddef41440166
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-07-28 00:18:20 -0400

    update hazard labels

--- SEQuential/R/SEQuential.R
+++ SEQuential/R/SEQuential.R
@@ -148,7 +148,7 @@ SEQuential <- function(data, id.col, time.col, eligible.col, treatment.col, outc
     }
   } else {
     for (i in seq_along(subgroups)) {
-      label <- paste0(params@subgroup, "_", subgroups[[i]])
+      label <- subgroups[[i]]
       models <- lapply(analytic, function(x) x$model[[i]])
       hazard[[label]] <- internal.hazard(models, params)
     }
