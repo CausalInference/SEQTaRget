436346fcd735aa31be17dec9e364f83041c1fcfa
Tom Palmer <remlapmot@hotmail.com>
2026-03-27 11:11:50 +0000

    Clear WDT in internal.analysis handler when data.return = FALSE to free memory earlier

--- SEQTaRget/R/internal_analysis.R
+++ SEQTaRget/R/internal_analysis.R
@@ -147,6 +147,7 @@ internal.analysis <- function(params) {
         }
         model <- internal.model(WDT, params)
       }
+      if (!params@data.return) WDT <- data.table()
       return(list(
         model = model,
         weighted_stats = if (params@weighted) stats else NA,
@@ -159,8 +160,6 @@ internal.analysis <- function(params) {
     full <- handler(params@DT, params@data, params)
     stopifnot(identical(nrow(params@DT), original_nrow))
     stopifnot(identical(names(params@DT), original_names))
-
-    if (!params@data.return) full$WDT <- data.table()
 
     if (params@bootstrap & params@verbose) cat("Bootstrapping with", params@bootstrap.sample * 100, "% of data", params@bootstrap.nboot, "times\n")
     UIDs <- unique(params@DT[[params@id]])
